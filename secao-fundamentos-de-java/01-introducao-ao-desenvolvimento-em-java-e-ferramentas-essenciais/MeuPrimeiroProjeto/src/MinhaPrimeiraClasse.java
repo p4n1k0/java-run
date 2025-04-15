8e9e9c398f9890eb7b907635d62a6ed2e1c01641
p4n1k0 <gabre.tarick@gmail.com>
2025-04-15 15:56:51 -0300

    inicio de tudo

--- secao-fundamentos-de-java/01-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuPrimeiroProjeto/src/MinhaPrimeiraClasse.java
+++ secao-fundamentos-de-java/01-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuPrimeiroProjeto/src/MinhaPrimeiraClasse.java
@@ -1,5 +1,6 @@
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.print("Primeira Linha");
+        System.out.print("Será impresso também na primeira linha");
     }
 }
