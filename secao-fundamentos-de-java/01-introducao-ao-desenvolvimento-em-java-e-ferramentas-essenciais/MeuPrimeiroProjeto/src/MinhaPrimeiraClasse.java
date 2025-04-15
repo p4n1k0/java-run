833dc454bc72f15eb675ec297a06b3538846cacd
p4n1k0 <gabre.tarick@gmail.com>
2025-04-15 16:03:28 -0300

    organizando classes em pacotes

--- secao-fundamentos-de-java/01-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuPrimeiroProjeto/src/MinhaPrimeiraClasse.java
+++
@@ -1,6 +0,0 @@
-public class MinhaPrimeiraClasse {
-    public static void main(String[] args) {
-        System.out.print("Primeira Linha");
-        System.out.print("Será impresso também na primeira linha");
-    }
-}
---
+++ secao-fundamentos-de-java/01-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuPrimeiroProjeto/src/com/tarick/minhaprimeiraclasse/MinhaPrimeiraClasse.java
@@ -0,0 +1,13 @@
+package com.tarick.minhaprimeiraclasse;
+
+import java.lang.*;
+import java.lang.Math;
+
+public class MinhaPrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("A raiz quadrada de 25 é:");
+        System.out.println(
+                Math.sqrt(25)
+        );
+    }
+}
