3531fde7abfe083035a0712753568531226bbcbe
p4n1k0 <gabre.tarick@gmail.com>
2025-04-15 16:06:18 -0300

    variaveis e tipos primitivos

--- secao-fundamentos-de-java/01-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuPrimeiroProjeto/src/com/tarick/minhaprimeiraclasse/MinhaPrimeiraClasse.java
+++ secao-fundamentos-de-java/01-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuPrimeiroProjeto/src/com/tarick/minhaprimeiraclasse/MinhaPrimeiraClasse.java
@@ -1,13 +1,11 @@
 package com.tarick.minhaprimeiraclasse;
 
-import java.lang.*;
-import java.lang.Math;
 
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println("A raiz quadrada de 25 é:");
-        System.out.println(
-                Math.sqrt(25)
-        );
+        int respostaParaTudo = 42;
+
+        System.out.println("A resposta para tudo é:");
+        System.out.println(respostaParaTudo);
     }
 }
