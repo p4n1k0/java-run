3ab3c3598bb0cbf11766451c22f14f4f9309bd91
p4n1k0 <gabre.tarick@gmail.com>
2025-04-15 16:57:15 -0300

    char

--- secao-fundamentos-de-java/01-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuPrimeiroProjeto/src/com/tarick/minhaprimeiraclasse/MinhaPrimeiraClasse.java
+++ secao-fundamentos-de-java/01-introducao-ao-desenvolvimento-em-java-e-ferramentas-essenciais/MeuPrimeiroProjeto/src/com/tarick/minhaprimeiraclasse/MinhaPrimeiraClasse.java
@@ -3,7 +3,9 @@ package com.tarick.minhaprimeiraclasse;
 
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
-        boolean isBoolean = true; // outros exemplos: isGreaterThan, isFlexible, isStarted
-        boolean ehNumero = false; // outros exemplos: ehMaiorDeIdade, ehCedo, ehValido
+        char letraA = 'a';
+        char letraAMaiuscula = 'A';
+        System.out.println(letraA); // a
+        System.out.println(letraAMaiuscula); // A
     }
 }
