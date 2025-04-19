01c228477ca8ea70d5e70f8068309b3aaa9c3207
p4n1k0 <gabre.tarick@gmail.com>
2025-04-19 14:15:09 -0300

    modifica classe p atributos privados e add getters e setters

--- secao-orientacao-a-objetos/01-abstracao-e-encapsulamento/MeuCarro/src/com/tarick/carro/Carro.java
+++ secao-orientacao-a-objetos/01-abstracao-e-encapsulamento/MeuCarro/src/com/tarick/carro/Carro.java
@@ -1,17 +1,32 @@
 package com.tarick.carro;
 
 public class Carro {
-  String marca;
-  String modelo;
-  int ano;
+  private String marca;
+  private String modelo;
+  private int ano;
 
-  public Carro(String marca, String modelo, int ano) {
-    this.marca = marca;
-    this.modelo = modelo;
-    this.ano = ano;
+
+  public String getMarca() {
+    return marca;
   }
 
-  public void exibirInformacoes() {
-    System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Ano: " + ano);
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
   }
 }
