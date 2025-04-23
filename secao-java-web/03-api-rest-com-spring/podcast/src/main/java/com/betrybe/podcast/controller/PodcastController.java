f0970343becc684883836538eab74c29cbd09dff
p4n1k0 <gabre.tarick@gmail.com>
2025-04-23 16:03:08 -0300

    rotas GET

--- secao-java-web/03-api-rest-com-spring/podcast/src/main/java/com/betrybe/podcast/controller/PodcastController.java
+++ secao-java-web/03-api-rest-com-spring/podcast/src/main/java/com/betrybe/podcast/controller/PodcastController.java
@@ -1,16 +1,28 @@
 package com.betrybe.podcast.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/podcasts")
 public class PodcastController {
 
-  @RequestMapping(method=RequestMethod.GET)
+  @GetMapping
   public String getRoot() {
     return "Yay podcasts!";
+  }
+
+  @GetMapping("/{id}")
+  public String getPodcast(@PathVariable long id) {
+    return "Você pediu pelo Podcast com id: %d".formatted(id);
+  }
+
+  @GetMapping("/search")
+  public String searchPodcast(@RequestParam String title) {
+    return "Você buscou por Podcasts com o título: %s".formatted(title);
   }
 }
