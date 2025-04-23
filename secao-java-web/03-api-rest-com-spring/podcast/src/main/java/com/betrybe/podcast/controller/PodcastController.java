e47e16f911d54f700cb59146c4f709c23bbd0898
p4n1k0 <gabre.tarick@gmail.com>
2025-04-23 15:57:48 -0300

    mapeamento de rotas

--- secao-java-web/03-api-rest-com-spring/podcast/src/main/java/com/betrybe/podcast/controller/PodcastController.java
+++ secao-java-web/03-api-rest-com-spring/podcast/src/main/java/com/betrybe/podcast/controller/PodcastController.java
@@ -2,13 +2,14 @@ package com.betrybe.podcast.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/podcasts")
 public class PodcastController {
 
-  @GetMapping
+  @RequestMapping(method=RequestMethod.GET)
   public String getRoot() {
     return "Yay podcasts!";
   }
