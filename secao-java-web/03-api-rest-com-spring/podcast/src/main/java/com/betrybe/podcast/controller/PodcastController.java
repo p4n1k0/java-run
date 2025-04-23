eb151ec79e726638a781b9d5a5420ef2e6edf5d5
p4n1k0 <gabre.tarick@gmail.com>
2025-04-23 18:47:56 -0300

    rota POST e outros mapeamentos

--- secao-java-web/03-api-rest-com-spring/podcast/src/main/java/com/betrybe/podcast/controller/PodcastController.java
+++ secao-java-web/03-api-rest-com-spring/podcast/src/main/java/com/betrybe/podcast/controller/PodcastController.java
@@ -3,9 +3,12 @@ package com.betrybe.podcast.controller;
 import com.betrybe.podcast.model.Podcast;
 import com.betrybe.podcast.service.PodcastService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,5 +42,12 @@ public class PodcastController {
   @GetMapping("/search")
   public String searchPodcast(@RequestParam String title) {
     return "Você buscou por Podcasts com o título: %s".formatted(title);
+  }
+
+  @PostMapping
+  public ResponseEntity<Podcast> createPodcast(@RequestBody Podcast newPodcast) {
+    Podcast savedPodcast = service.savePodcast(newPodcast);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedPodcast);
   }
 }
