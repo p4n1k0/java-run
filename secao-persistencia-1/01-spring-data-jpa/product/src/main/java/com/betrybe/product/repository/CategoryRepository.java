e796811e9ce9da768a17d2f4699c18dce772e190
p4n1k0 <gabre.tarick@gmail.com>
2025-04-26 17:40:33 -0300

    repositorios

---
+++ secao-persistencia-1/01-spring-data-jpa/product/src/main/java/com/betrybe/product/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.betrybe.product.repository;
+
+import com.betrybe.product.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
