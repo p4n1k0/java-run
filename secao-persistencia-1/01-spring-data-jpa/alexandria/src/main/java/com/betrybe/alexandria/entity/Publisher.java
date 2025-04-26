95c45336ef559f5b24fd4e639165e7b8356d9cf0
p4n1k0 <gabre.tarick@gmail.com>
2025-04-26 15:14:20 -0300

    anotando entidades

--- secao-persistencia-1/01-spring-data-jpa/alexandria/src/main/java/com/betrybe/alexandria/entity/Publisher.java
+++ secao-persistencia-1/01-spring-data-jpa/alexandria/src/main/java/com/betrybe/alexandria/entity/Publisher.java
@@ -1,7 +1,17 @@
 package com.betrybe.alexandria.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "publishers")
 public class Publisher {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
