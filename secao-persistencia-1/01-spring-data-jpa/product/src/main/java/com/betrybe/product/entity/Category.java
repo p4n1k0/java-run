ce2cae86f38b827da00faa7de4055296357bc141
p4n1k0 <gabre.tarick@gmail.com>
2025-04-26 17:34:05 -0300

    anota entidades

--- secao-persistencia-1/01-spring-data-jpa/product/src/main/java/com/betrybe/product/entity/Category.java
+++ secao-persistencia-1/01-spring-data-jpa/product/src/main/java/com/betrybe/product/entity/Category.java
@@ -1,8 +1,19 @@
 package com.betrybe.product.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
 public class Category {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String name;
 
   public Category() {}
