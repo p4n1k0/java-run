5d947d28f21dddaf79f8f1148830e4d815dc87bc
p4n1k0 <gabre.tarick@gmail.com>
2025-04-26 17:57:33 -0300

    Product service

--- secao-persistencia-1/01-spring-data-jpa/product/src/main/java/com/betrybe/product/service/ProductService.java
+++ secao-persistencia-1/01-spring-data-jpa/product/src/main/java/com/betrybe/product/service/ProductService.java
@@ -2,6 +2,8 @@ package com.betrybe.product.service;
 
 import com.betrybe.product.entity.Product;
 import com.betrybe.product.repository.ProductRepository;
+import com.betrybe.product.service.exception.ProductNotFoundException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,33 @@ public class ProductService {
     this.productRepository = productRepository;
   }
 
-  public Product findById(Long id)
+  public Product findById(Long id) throws ProductNotFoundException {
+    return productRepository.findById(id)
+        .orElseThrow(ProductNotFoundException::new);
+  }
+
+  public List<Product> findAll() {
+    return productRepository.findAll();
+  }
+
+  public Product create(Product product) {
+    return productRepository.save(product);
+  }
+
+  public Product update(Long id, Product product) throws ProductNotFoundException {
+    Product productFromDb = findById(id);
+
+    productFromDb.setName(product.getName());
+    productFromDb.setPrice(product.getPrice());
+
+    return productRepository.save(productFromDb);
+  }
+
+  public Product deleteById(Long id) throws ProductNotFoundException {
+    Product product = findById(id);
+
+    productRepository.deleteById(id);
+
+    return product;
+  }
 }
