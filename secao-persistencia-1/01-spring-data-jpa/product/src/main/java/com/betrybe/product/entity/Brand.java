1991bf77ec51711498f6bde49236231198b34467
p4n1k0 <gabre.tarick@gmail.com>
2025-04-26 17:30:11 -0300

    entidades

---
+++ secao-persistencia-1/01-spring-data-jpa/product/src/main/java/com/betrybe/product/entity/Brand.java
@@ -0,0 +1,29 @@
+package com.betrybe.product.entity;
+
+public class Brand {
+
+  private Long id;
+  private String name;
+
+  public Brand() {}
+
+  public Brand(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
