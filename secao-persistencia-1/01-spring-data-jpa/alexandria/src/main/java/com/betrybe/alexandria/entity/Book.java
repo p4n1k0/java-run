0c121df8252da17cfc198d730cd9486478ae71c7
p4n1k0 <gabre.tarick@gmail.com>
2025-04-27 16:24:49 -0300

    inicia relacionamento N p M

--- secao-persistencia-1/01-spring-data-jpa/alexandria/src/main/java/com/betrybe/alexandria/entity/Book.java
+++ secao-persistencia-1/01-spring-data-jpa/alexandria/src/main/java/com/betrybe/alexandria/entity/Book.java
@@ -6,9 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "books")
@@ -27,6 +30,14 @@ public class Book {
   @ManyToOne
   @JoinColumn(name = "publisher_id")
   private Publisher publisher;
+
+  @ManyToMany
+  @JoinTable(
+      name = "authors_books",
+      joinColumns = @JoinColumn(name = "book_id"),
+      inverseJoinColumns = @JoinColumn(name = "author_id")
+  )
+  private List<Author> authors;
 
   public Book() {}
 
@@ -73,5 +84,13 @@ public class Book {
 
   public void setPublisher(Publisher publisher) {
     this.publisher = publisher;
+  }
+
+  public List<Author> getAuthors() {
+    return authors;
+  }
+
+  public void setAuthors(List<Author> authors) {
+    this.authors = authors;
   }
 }
