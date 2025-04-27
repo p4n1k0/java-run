9179e3de9f04c8a099b76da1217ddeebb006f9c1
p4n1k0 <gabre.tarick@gmail.com>
2025-04-27 15:57:47 -0300

    refatora controller Book

--- secao-persistencia-1/01-spring-data-jpa/alexandria/src/main/java/com/betrybe/alexandria/controller/BookController.java
+++ secao-persistencia-1/01-spring-data-jpa/alexandria/src/main/java/com/betrybe/alexandria/controller/BookController.java
@@ -8,6 +8,7 @@ import com.betrybe.alexandria.entity.Book;
 import com.betrybe.alexandria.service.BookService;
 import com.betrybe.alexandria.service.exception.BookDetailNotFoundException;
 import com.betrybe.alexandria.service.exception.BookNotFoundException;
+import com.betrybe.alexandria.service.exception.PublisherNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -91,6 +92,21 @@ public class BookController {
       throws BookNotFoundException, BookDetailNotFoundException {
     return BookDetailDto.fromEntity(
         bookService.deleteBookDetail(bookId)
+    );
+  }
+
+  @PutMapping("{bookId}/publisher/{publisherId}")
+  public BookDto setBookPublisher(@PathVariable Long bookId,
+      @PathVariable Long publisherId) throws BookNotFoundException, PublisherNotFoundException {
+    return BookDto.fromEntity(
+        bookService.setBookPublisher(bookId, publisherId)
+    );
+  }
+
+  @DeleteMapping("/{bookId}/publisher")
+  public BookDto deleteBookPublisher(@PathVariable Long bookId) throws BookNotFoundException {
+    return BookDto.fromEntity(
+        bookService.deleteBookPublisher(bookId)
     );
   }
 }
