79bacd275cb6710a3bd0e5fa51a7c71ee8e0c915
p4n1k0 <gabre.tarick@gmail.com>
2025-04-27 15:48:08 -0300

    inicia relacionamento 1 p N

--- secao-persistencia-1/01-spring-data-jpa/alexandria/src/main/java/com/betrybe/alexandria/entity/Book.java
+++ secao-persistencia-1/01-spring-data-jpa/alexandria/src/main/java/com/betrybe/alexandria/entity/Book.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -21,6 +23,10 @@ public class Book {
 
   @OneToOne(cascade = CascadeType.ALL, mappedBy = "book")
   private BookDetail details;
+
+  @ManyToOne
+  @JoinColumn(name = "publisher_id")
+  private Publisher publisher;
 
   public Book() {}
 
@@ -59,5 +65,13 @@ public class Book {
 
   public void setDetails(BookDetail details) {
     this.details = details;
+  }
+
+  public Publisher getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(Publisher publisher) {
+    this.publisher = publisher;
   }
 }
